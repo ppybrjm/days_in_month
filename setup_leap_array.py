d723521d9c9dc76747d2010fa9adbcff1767b85d
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-06-05 14:56:18 +0100

    More definitions, some calcuated on Class, others on Variable

--- setup_leap_array.py
+++ setup_leap_array.py
@@ -12,4 +12,38 @@ class gregorian_patern_def:
     MID_PATERN = [1]*25       # Every 100 years, miss a leap year
     MID_PATERN[24] = 0
     LONG_PATERN = [0,0,0,1]   # Every 400 years, add a bonus leap year.
+
     YEARS_IN_CYCLE = len(SHORT_PATERN) * len(MID_PATERN) * len(LONG_PATERN)
+    MONTHS_IN_CYCLE = YEARS_IN_CYCLE * year_cycle.MONTHS_IN_YEAR
+
+    def __init__(self):
+        self.LONG_CYCLE = self.define_gregorian_long_cycle()
+        self.DAYS_IN_LONG_CYCLE = self.count_days_in_long_cycle()
+
+    def define_gregorian_long_cycle(self):
+        years_in_long_cycle = []
+
+        for century in gregorian_patern_def.LONG_PATERN:
+            for quadrennial in gregorian_patern_def.MID_PATERN:
+                for viewed_year in gregorian_patern_def.SHORT_PATERN:
+                    if viewed_year == 1 and (quadrennial == 1 or century == 1):
+                        years_in_long_cycle.append(1)
+                    else:
+                        years_in_long_cycle.append(0)
+
+        return years_in_long_cycle
+
+    def count_days_in_long_cycle(self):
+        years = len(self.LONG_CYCLE)
+        non_leap_days = years * year_cycle.SHORT_YEAR_DAYS
+        leap_days = sum(self.LONG_CYCLE)
+        return non_leap_days + leap_days
+        
+
+
+print(gregorian_patern_def.MONTHS_IN_CYCLE)
+
+cycle = gregorian_patern_def()
+print(cycle.LONG_PATERN)
+print(cycle.LONG_CYCLE)
+print(cycle.DAYS_IN_LONG_CYCLE)
