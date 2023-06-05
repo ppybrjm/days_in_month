141c3f6ff08a07c66171d2772716d1edfca2f4a2
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-06-05 14:39:26 +0100

    COMMIT 3.1 - Previous didn't work, this now works

--- setup_leap_array.py
+++ setup_leap_array.py
@@ -1,22 +1,15 @@
 class year_cycle:
-    MONTH_CYCLE = [31,28,31,30,31,30,31,31,30,31,30,31]
-    MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    MONTH_CYCLE_NAMES = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    MONTH_CYCLE_DAYS = [31,28,31,30,31,30,31,31,30,31,30,31]
     LEAP_DAY_INDEX = 1 #Leap Day in Feb
-    MONTHS_IN_YEAR = 12
+
+    MONTHS_IN_YEAR = len(MONTH_CYCLE_NAMES)
+    SHORT_YEAR_DAYS = sum(MONTH_CYCLE_DAYS)
+    LEAP_YEAR_DAYS = SHORT_YEAR_DAYS + 1
 
 class gregorian_patern_def:
     SHORT_PATERN = [0,0,0,1]  # Leap year every 4 years
     MID_PATERN = [1]*25       # Every 100 years, miss a leap year
     MID_PATERN[24] = 0
     LONG_PATERN = [0,0,0,1]   # Every 400 years, add a bonus leap year.
-
-class defining_constants:
-    @staticmethod
-    def define_short_year(MONTH_CYCLE):
-        days = 0
-        for month in MONTH_CYCLE:
-            days += month if month != "A" else 28
-        return days
-
-    SHORT_YEAR = define_short_year(year_cycle.MONTH_CYCLE)
-    LEAP_YEAR = SHORT_YEAR+1
+    YEARS_IN_CYCLE = len(SHORT_PATERN) * len(MID_PATERN) * len(LONG_PATERN)
