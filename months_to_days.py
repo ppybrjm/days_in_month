0a03ef9142ba531ed680021b14605bde87feae09
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-06-06 12:33:01 +0100

    Added object for processing Years (No Processing happening yet)

--- months_to_days.py
+++ months_to_days.py
@@ -1,7 +1,10 @@
 from setup_leap_array import year_cycle, gregorian_cycle_def
 
 def months_to_days(number_of_months: int):
-    months_calculater = months_calc(number_of_months)
+    if number_of_months >=  year_cycle.MONTHS_IN_YEAR:
+        months_calculater = years_months_calc(number_of_months)
+    else:
+        months_calculater = months_calc(number_of_months)
 
     print(months_calculater)
 
@@ -13,6 +16,25 @@ class months_calc():
         plural_months = "" if self.number_of_months == 1 else "s"
         return("processing {} month{}".format(self.number_of_months, plural_months))
 
+
+class years_months_calc(months_calc):
+    def __init__(self, number_of_months):
+        super().__init__(number_of_months)
+        self.year_total_number = self.number_of_months // year_cycle.MONTHS_IN_YEAR
+        self.month_remainder_number = self.number_of_months % year_cycle.MONTHS_IN_YEAR
+
+
+    def __str__(self):
+        parent_str = super().__str__()
+
+        purual_years = "" if self.year_total_number == 1 else "s"
+        plural_months = "" if self.month_remainder_number == 1 else "s"
+
+        return("{} = {} year{} {} month{}".format(
+            parent_str,
+            self.year_total_number, purual_years, 
+            self.month_remainder_number, plural_months
+            ))
 
 
 months_to_days(1)
