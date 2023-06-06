362891561ffd4aed336cbe1fab3b67cbd7a22308
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-06-06 14:54:26 +0100

    Update Test to use new function

--- test_process_str.py
+++ test_process_str.py
@@ -48,4 +48,4 @@ test_cases = [
 
 for test in test_cases:
         calc_object = get_month_calc_object(test["months"])
-        assert(calc_object.__str__() == test["process"])
+        assert(calc_object.generate_processing_str() == test["process"])
